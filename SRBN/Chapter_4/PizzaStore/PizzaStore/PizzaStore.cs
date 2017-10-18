0733955653b2eee9b7523f2e15d800d50720a78b
Serhii Bihun <stalkerok1993@gmail.com>
2017-10-18 09:06:19 +0300

    Add class diagram for Simple Factory principle.

--- SRBN/Chapter_4/PizzaStore/PizzaStore/PizzaStore.cs
+++ SRBN/Chapter_4/PizzaStore/PizzaStore/PizzaStore.cs
@@ -1,10 +1,6 @@
 ﻿using PizzaStore.Factory;
 using PizzaStore.Pizza;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PizzaStore
 {
