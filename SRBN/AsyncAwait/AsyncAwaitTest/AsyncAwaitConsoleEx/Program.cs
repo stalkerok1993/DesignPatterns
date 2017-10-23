f017b74d4feb7cb6e41f87a3b137d827de281a70
Serhii Bihun <stalkerok1993@gmail.com>
2017-10-23 10:20:38 +0300

    Add link to library that is helps to use async/await in console.

--- SRBN/AsyncAwait/AsyncAwaitTest/AsyncAwaitConsoleEx/Program.cs
+++ SRBN/AsyncAwait/AsyncAwaitTest/AsyncAwaitConsoleEx/Program.cs
@@ -9,10 +9,26 @@ using AsyncAwaitTest.SimpleLogWriters;
 namespace AsyncAwaitConsoleEx {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine($"Has SynchronizationContext? {SynchronizationContext.Current != null}");
+            Console.WriteLine($"1Has SynchronizationContext? {SynchronizationContext.Current != null}");
+
+            // see RUN method in https://github.com/StephenCleary/AsyncEx/blob/master/src/Nito.AsyncEx.Context/AsyncContext.cs
+            //var _taskFactory = new TaskFactory(CancellationToken.None, TaskCreationOptions.HideScheduler, TaskContinuationOptions.HideScheduler, _taskScheduler);
+            //_taskFactory.
+            StartWithContext();
+
+            GC.Collect();
+            Thread.Sleep(2000);
+            Console.WriteLine($"5Has SynchronizationContext? {SynchronizationContext.Current != null}");
+
+            Console.WriteLine("Press any key to continue . . .");
+            Console.ReadKey();
+        }
+
+        private static void StartWithContext() {
+            Console.WriteLine($"2Has SynchronizationContext? {SynchronizationContext.Current != null}");
             using (new DummyForm()) {
                 //SynchronizationContext.SetSynchronizationContext(new WindowsFormsSynchronizationContext());
-                Console.WriteLine($"Has SynchronizationContext? {SynchronizationContext.Current != null}");
+                Console.WriteLine($"3Has SynchronizationContext? {SynchronizationContext.Current != null}");
 
                 ThreadLogger logger = new ThreadLogger(new ConsoleLogWriter());
                 logger.LogMethodNumber = 4;
@@ -23,25 +39,9 @@ namespace AsyncAwaitConsoleEx {
                 test.Execute();
                 //new Thread(StartWithContext).Start(SynchronizationContext.Current);
 
+                Console.WriteLine("Worker thread exited.");
             }
-            GC.Collect();
-            Thread.Sleep(2000);
-            Console.WriteLine($"Has SynchronizationContext? {SynchronizationContext.Current != null}");
-
-            Console.WriteLine("Press any key to continue . . .");
-            //Console.ReadKey();
-        }
-
-        private static void StartWithContext(object context) {
-            ThreadLogger logger = new ThreadLogger(new ConsoleLogWriter());
-            logger.LogMethodNumber = 4;
-            var test = new AATest(logger);
-            test.UseInnerLogMethodNames = true;
-
-            // WTF with this execution? See: https://habrahabr.ru/post/232169/ and https://msdn.microsoft.com/en-us/magazine/gg598924.aspx
-            Task.Factory.StartNew(test.Execute, CancellationToken.None, TaskCreationOptions.None, TaskScheduler.FromCurrentSynchronizationContext());
-
-            Console.WriteLine($"Has SynchronizationContext? {SynchronizationContext.Current != null}");
+            Console.WriteLine($"4Has SynchronizationContext? {SynchronizationContext.Current != null}");
         }
     }
 }
